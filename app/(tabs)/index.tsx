7c79e3e7bdf63e15032e67467444568c560fa9d1
Dennis Yildirim <dennis@yld.im>
2025-06-18 18:20:01 +0200

    fix: changed feed top margin

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ marginTop: 40 }}>
+    <View style={{ marginTop: 50 }}>
     <RSSFeed />
     </View>
   )
